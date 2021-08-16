0a4f0de8af802040238809b2ee2f1f44c1ff75ce
russelj <russelj@rose-hulman.edu>
2021-08-15 20:42:55 -0400

    Buddy Forum is Functional

--- Buddy-Buddy/public/scripts/main.js
+++ Buddy-Buddy/public/scripts/main.js
@@ -11,16 +11,20 @@ var rhit = rhit || {};
 rhit.FB_CHATS_COLLECTION = "Chats";
 rhit.FB_KEY_MESSAGE = "message";
 rhit.FB_KEY_SENDER = "sender";
-rhit.FB_KEY_TIMESTAMP = "lastTouched";
+rhit.FB_KEY_LAST_TOUCHED = "lastTouched";
 rhit.FB_FORUM_COLLECTION = "Forum";
 rhit.FB_USERS_COLLECTION = "Users";
+rhit.FB_KEY_AUTHOR = "author";
+rhit.FB_KEY_TITLE = "title";
+rhit.FB_KEY_BODY = "body";
 
 let map;
-
 
 rhit.fbAuthManager = null;
 rhit.fbUsersManager = null;
 rhit.fbChatsManager = null;
+rhit.fbForumManager = null;
+rhit.fbSinglePostManager = null;
 
 // From https://stackoverflow.com/questions/494143/creating-a-new-dom-element-from-an-html-string-using-built-in-dom-methods-or-pro
 function htmlToElement(html) {
@@ -28,6 +32,250 @@ function htmlToElement(html) {
 	html = html.trim();
 	template.innerHTML = html;
 	return template.content.firstChild;
+}
+
+rhit.ForumListController = class {
+	constructor(){
+		document.querySelector("#menuFindBuddy").onclick = (event) => {
+			window.location.href = "/Find Buddy.html";
+		};
+		document.querySelector("#menuFindRoute").onclick = (event) => {
+			window.location.href = "/Find Route.html";
+		};
+		document.querySelector("#menuGoToHomePage").onclick = (event) => {
+			window.location.href = "/Homepage.html";
+		};
+		document.querySelector("#menuForum").onclick = (event) => {
+			window.location.href = "/Buddy Forum.html";
+		};
+		document.querySelector("#menuChat").onclick = (event) => {
+			window.location.href = `/Chat.html?uid=${rhit.fbAuthManager.uid}`;
+		};
+		document.querySelector("#menuGoToProfilePage").onclick = (event) => {
+			window.location.href = "/Profile.html";
+		}
+		document.querySelector("#menuSignOut").onclick = (event) => {
+			rhit.fbAuthManager.signOut();
+			window.location.href = "/index.html";
+		}
+
+		document.querySelector("#submitForumPost").addEventListener("click", (event) => {
+			const title = document.querySelector("#postTitle").value;
+			const body = document.querySelector("#postBody").value;
+			const author = rhit.fbAuthManager.uid;
+			rhit.fbForumManager.add(title, body, author);
+		});
+
+		$("#addForum").on("show.bs.modal", (event) => {
+			// Pre animation
+			document.querySelector("#postTitle").value = "";
+			document.querySelector("#postBody").value = "";
+		});
+		$("#addForum").on("shown.bs.modal", (event) => {
+			// Post animation
+			document.querySelector("#postTitle").focus();
+		});
+
+		rhit.fbForumManager.beginListening(this.updateList.bind(this));
+	}
+
+	updateList() {
+		console.log("I need to update the list on the page!");
+		console.log(`Num posts = ${rhit.fbForumManager.length}`);
+		console.log("Example post = ", rhit.fbForumManager.getPostIndex(0));
+
+
+		const newList = htmlToElement('<div id="forumListContainer"></div>');
+		
+		for (let i = 0; i < rhit.fbForumManager.length; i++) {
+			const mq = rhit.fbForumManager.getPostIndex(i);
+			const newCard = this._createCard(mq);
+			newCard.onclick = (event) => {
+				window.location.href = `/PostDetail.html?id=${mq.id}`;
+			};
+			newList.appendChild(newCard);
+		}
+
+		const oldList = document.querySelector("#forumListContainer");
+		oldList.removeAttribute("id");
+		oldList.hidden = true;
+		// Put in the new quoteListContainer
+		oldList.parentElement.appendChild(newList);
+	}
+
+	_createCard(post) {
+		return htmlToElement(`<div class="card mx-auto forumPost">
+		<div class="card-body">
+		  <h4 class="card-title">${post.title}</h4>
+		  <p class="card-text">${post.body}</p>
+		  <p class="card-author">By ${post.author}</p>
+		</div>
+	  </div>`);
+	}
+}
+
+rhit.Post = class {
+	constructor(id, title, body, author) {
+		this.id = id;
+		this.title = title;
+		this.body = body;
+		this.author = author;
+	}
+}
+
+rhit.FbForumManager = class {
+	constructor(uid){
+		this._uid = uid;
+		this._documentSnapshots = [];
+		this._ref = firebase.firestore().collection(rhit.FB_FORUM_COLLECTION);
+		this._unsubscribe = null;
+	}
+
+	add(title, body, author) {
+		// Add a new document with a generated id.
+		this._ref.add({
+				[rhit.FB_KEY_TITLE]: title,
+				[rhit.FB_KEY_BODY]: body,
+				[rhit.FB_KEY_AUTHOR]: author,
+				[rhit.FB_KEY_LAST_TOUCHED]: firebase.firestore.Timestamp.now(),
+			})
+			.then(function (docRef) {
+				console.log("Document written with ID: ", docRef.id);
+			})
+			.catch(function (error) {
+				console.error("Error adding document: ", error);
+			});
+	}
+
+	beginListening(changeListener) {
+
+		let query = this._ref.orderBy(rhit.FB_KEY_LAST_TOUCHED, "desc").limit(50);
+		if (this._uid) {
+				query = query.where(rhit.FB_KEY_AUTHOR, "==", this._uid);
+		}
+		
+		this._unsubscribe = query.onSnapshot((querySnapshot) => {
+				console.log("Post update!");
+				this._documentSnapshots = querySnapshot.docs;
+				changeListener();
+			});
+	}
+
+	stopListening() {
+		this._unsubscribe();
+	}
+
+	get length() {
+		return this._documentSnapshots.length;
+	}
+
+	getPostIndex(index) {
+		const docSnapshot = this._documentSnapshots[index];
+		const mq = new rhit.Post(docSnapshot.id,
+			docSnapshot.get(rhit.FB_KEY_TITLE),
+			docSnapshot.get(rhit.FB_KEY_BODY),
+			docSnapshot.get(rhit.FB_KEY_AUTHOR));
+		return mq;
+	}
+}
+
+rhit.ForumDetailPageController = class {
+	constructor() {
+
+		document.querySelector("#submitEditPost").addEventListener("click", (event) => {
+			const title = document.querySelector("#inputTitle").value;
+			const body = document.querySelector("#inputBody").value;
+			const author = rhit.fbAuthManager.uid;
+			rhit.fbSinglePostManager.update(title, body, author);
+		});
+
+		$("#editPostDialog").on("show.bs.modal", (event) => {
+			// Pre animation
+			document.querySelector("#inputTitle").value = rhit.fbSinglePostManager.title;
+			document.querySelector("#inputBody").value = rhit.fbSinglePostManager.body;
+		});
+		$("#editPostDialog").on("shown.bs.modal", (event) => {
+			// Post animation
+			document.querySelector("#inputTitle").focus();
+		});
+
+		document.querySelector("#submitDeletePost").addEventListener("click", event => {
+			rhit.fbSinglePostManager.delete().then(() => {
+				console.log("Document successfully deleted!");
+				window.location.href = "/Buddy Forum.html";
+			}).catch(error => console.error("Error removing document: ", error));
+		});
+
+		rhit.fbSinglePostManager.beginListening(this.updateView.bind(this));
+	}
+
+	updateView() {
+		document.querySelector("#cardTitle").innerHTML = rhit.fbSinglePostManager.title;
+		document.querySelector("#cardBody").innerHTML = rhit.fbSinglePostManager.body;
+		document.querySelector("#cardAuthor").innerHTML = rhit.fbSinglePostManager.author;
+		if (rhit.fbSinglePostManager.author == rhit.fbAuthManager.uid) {
+			document.querySelector("#menuEdit").style.display = "flex";
+			document.querySelector("#menuDelete").style.display = "flex";
+		}
+	}
+}
+
+rhit.FbSinglePostManager = class {
+	constructor(postId) {
+		this._documentSnapshot = {};
+		this._unsubscribe = null;
+		this._ref = firebase.firestore().collection(rhit.FB_FORUM_COLLECTION).doc(postId);
+	}
+
+	beginListening(changeListener) {
+		this._unsubscribe = this._ref.onSnapshot((doc) => {
+			if (doc.exists) {
+				console.log("Document data:", doc.data());
+				this._documentSnapshot = doc;
+				changeListener();
+			} else {
+				// doc.data() will be undefined in this case
+				console.log("No such document!");
+				//window.location.href = "/";
+			}
+		});
+	}
+
+	stopListening() {
+		this._unsubscribe();
+	}
+
+	update(title, body, author) {
+		this._ref.update({
+				[rhit.FB_KEY_TITLE]: title,
+				[rhit.FB_KEY_BODY]: body,
+				[rhit.FB_KEY_AUTHOR]: author,
+				[rhit.FB_KEY_LAST_TOUCHED]: firebase.firestore.Timestamp.now(),
+			})
+			.then(() => {
+				console.log("Document successfully updated!");
+			})
+			.catch(function (error) {
+				// The document probably doesn't exist.
+				console.error("Error updating document: ", error);
+			});
+	}
+
+	delete() {
+		return this._ref.delete();
+	}
+
+	get title() {
+		return this._documentSnapshot.get(rhit.FB_KEY_TITLE);
+	}
+
+	get body() {
+		return this._documentSnapshot.get(rhit.FB_KEY_BODY);
+	}
+
+	get author() {
+		return this._documentSnapshot.get(rhit.FB_KEY_AUTHOR);
+	}
 }
 
 rhit.LoginPageController = class {
@@ -170,11 +418,7 @@ rhit.FbChatsManager = class {
 	}
 
 }
-rhit.ForumPageController = class {
-	constructor() {
-		rhit.HandleDrawerButtons();
-	}
-}
+
 rhit.FindBuddyPageController = class {
 	constructor() {
 		rhit.HandleDrawerButtons();
@@ -324,7 +568,18 @@ rhit.initializePage = function () {
 	}
 	if (document.querySelector("#forumPage")) {
 		console.log("You are on the forum page.");
-		new rhit.ForumPageController();
+		const uid = urlParams.get("uid");
+		rhit.fbForumManager = new rhit.FbForumManager(uid);
+		new rhit.ForumListController();
+	}
+	if (document.querySelector("#singlePostPage")) {
+		console.log("You are on the detail page.");
+		const postId = urlParams.get("id");
+		if (!postId) {
+			window.location.href = "/";
+		}
+		rhit.fbSinglePostManager = new rhit.FbSinglePostManager(postId);
+		new rhit.ForumDetailPageController();
 	}
 	if (document.querySelector("#accountPage")) {
 		console.log("You are on the profile page.");
