ef5e6ef8cc1811a52faa18f1cacdd04263ef4771
russelj <russelj@rose-hulman.edu>
2021-08-18 18:22:06 -0400

    merge conflicts solved lol

--- Buddy-Buddy/public/scripts/main.js
+++ Buddy-Buddy/public/scripts/main.js
@@ -37,128 +37,241 @@ function htmlToElement(html) {
 	return template.content.firstChild;
 }
 
-rhit.ProfilePageController = class {
-	constructor(){
-		document.querySelector("#submitEditAccount").addEventListener("click", (event) => {
-			const name = document.querySelector("#editInputName").value;
-			const location = document.querySelector("#editInputLocation").value;
-			const url = document.querySelector("#editInputURL").value;
-			rhit.fbAccountManager.add(name, location, url);
-		});
+// rhit.ProfilePageController = class {
+// 	constructor(){
+// 		document.querySelector("#submitEditAccount").addEventListener("click", (event) => {
+// 			const name = document.querySelector("#editInputName").value;
+// 			const location = document.querySelector("#editInputLocation").value;
+// 			const url = document.querySelector("#editInputURL").value;
+// 			rhit.fbAccountManager.add(name, location, url);
+// 		});
 	
-		$("#editAccountDialog").on("show.bs.modal", (event) => {
-			// Pre animation
-			document.querySelector("#editInputName").value = rhit.fbAccountManager.name;
-			document.querySelector("#editInputLocation").value = rhit.fbAccountManager.location;
-			document.querySelector("#editInputURL").value = rhit.fbAccountManager.url;
-		});
-		$("#editAccountDialog").on("shown.bs.modal", (event) => {
-			// Post animation
-			document.querySelector("#editInputName").focus();
-		});
+// 		$("#editAccountDialog").on("show.bs.modal", (event) => {
+// 			// Pre animation
+// 			document.querySelector("#editInputName").value = rhit.fbAccountManager.name;
+// 			document.querySelector("#editInputLocation").value = rhit.fbAccountManager.location;
+// 			document.querySelector("#editInputURL").value = rhit.fbAccountManager.url;
+// 		});
+// 		$("#editAccountDialog").on("shown.bs.modal", (event) => {
+// 			// Post animation
+// 			document.querySelector("#editInputName").focus();
+// 		});
 
-		rhit.fbAccountManager.beginListening(this.updateView.bind(this));
+// 		rhit.fbAccountManager.beginListening(this.updateView.bind(this));
 
-	}
-	updateView() {
-		const newList = htmlToElement('<div id="accountPage"></div>');
-		const mq = rhit.fbAccountManager.getAccount();
-		const newCard = this._createCard(mq);
-		newList.appendChild(newCard);
+// 	}
+// 	updateView() {
+// 		const newList = htmlToElement('<div id="accountPage"></div>');
+// 		const mq = rhit.fbAccountManager.getAccount();
+// 		const newCard = this._createCard(mq);
+// 		newList.appendChild(newCard);
 
-		const oldList = document.querySelector("#accountPage");
-		oldList.removeAttribute("id");
-		oldList.hidden = true;
-		oldList.parentElement.appendChild(newList);
+// 		const oldList = document.querySelector("#accountPage");
+// 		oldList.removeAttribute("id");
+// 		oldList.hidden = true;
+// 		oldList.parentElement.appendChild(newList);
 
-		document.querySelector("#accountUrl").innerHTML = `<img class="img-fluid card-img-top" style="margin: 20px; max-height: 375px;" src="${rhit.fbAccountManager.url}">`;
-		document.querySelector("#accountName").innerHTML = rhit.fbAccountManager.name;
-		document.querySelector("#accountEmail").innerHTML = rhit.fbAccountManager.email;
-		document.querySelector("#accountSLocation").innerHTML = rhit.fbAccountManager.url;
-	}
-	_createCard(account) {
-		return htmlToElement(`<div class="card mx-auto profile-style">
-		<div class="card-body">
-			<div id="accountUrl"><img style="margin: 20px; max-height: 375px;" class="card-img-top"
-	  src="${account.url}"
-	  alt="Your profile picture"></div>
-			<h4 id="${account.name}"></h4>
-			<p id="${account.id}@rose-hulman.edu" class="card-text"></p>
-			<p id="${account.location}" class="card-text"></p>
-		</div>
-	</div>`);
-	}
+// 		document.querySelector("#accountUrl").innerHTML = `<img class="img-fluid card-img-top" style="margin: 20px; max-height: 375px;" src="${rhit.fbAccountManager.url}">`;
+// 		document.querySelector("#accountName").innerHTML = rhit.fbAccountManager.name;
+// 		document.querySelector("#accountEmail").innerHTML = rhit.fbAccountManager.email;
+// 		document.querySelector("#accountSLocation").innerHTML = rhit.fbAccountManager.url;
+// 	}
+// 	_createCard(account) {
+// 		return htmlToElement(`<div class="card mx-auto profile-style">
+// 		<div class="card-body">
+// 			<div id="accountUrl"><img style="margin: 20px; max-height: 375px;" class="card-img-top"
+// 	  src="${account.url}"
+// 	  alt="Your profile picture"></div>
+// 			<h4 id="${account.name}"></h4>
+// 			<p id="${account.id}@rose-hulman.edu" class="card-text"></p>
+// 			<p id="${account.location}" class="card-text"></p>
+// 		</div>
+// 	</div>`);
+// 	}
 
-}
+// }
 
-rhit.Account = class {
-	constructor(name, location, url){
-		this.name = name;
-		this.url = url;
-		this.location = location;
-	}
-}
+// rhit.Account = class {
+// 	constructor(name, location, url){
+// 		this.name = name;
+// 		this.url = url;
+// 		this.location = location;
+// 	}
+// }
 
-rhit.FbAccountManager = class {
-	constructor(uid) {
-		rhit.HandleDrawerButtons();
-		this._documentSnapshot = {};
-		this._unsubscribe = null;
-		this._ref = firebase.firestore().collection(rhit.FB_USERS_COLLECTION).doc(uid);
-	}
+// rhit.FbAccountManager = class {
+// 	constructor(uid) {
+// 		rhit.HandleDrawerButtons();
+// 		this._documentSnapshot = {};
+// 		this._unsubscribe = null;
+// 		this._ref = firebase.firestore().collection(rhit.FB_USERS_COLLECTION).doc(uid);
+// 	}
 
-	beginListening(changeListener) {
-		this._unsubscribe = this._ref.onSnapshot((doc) => {
-			if (doc.exists) {
-				console.log("Document data:", doc.data());
-				this._documentSnapshot = doc;
-				changeListener();
-			} else {
-				console.log("No such document!");
-			}
-		});
-	}
+// 	beginListening(changeListener) {
+// 		this._unsubscribe = this._ref.onSnapshot((doc) => {
+// 			if (doc.exists) {
+// 				console.log("Document data:", doc.data());
+// 				this._documentSnapshot = doc;
+// 				changeListener();
+// 			} else {
+// 				console.log("No such document!");
+// 			}
+// 		});
+// 	}
 
-	stopListening() {
-		this._unsubscribe();
-	}
+// 	stopListening() {
+// 		this._unsubscribe();
+// 	}
 
-	add(name, location, url) {
-		this._ref.add({
-			[rhit.FB_KEY_NAME]: name,
-			[rhit.FB_KEY_AUTHOR]: rhit.fbAuthManager.uid,
-			[rhit.FB_KEY_LOCATION]: location,
-			[rhit.FB_KEY_URL]: url,
-		})
-		.then(() => {
-			console.log("Document successfully updated!");
-		})
-		.catch(function (error) {
-			console.error("Error updating document: ", error);
-		});
-	}
+// 	add(name, location, url) {
+// 		this._ref.add({
+// 			[rhit.FB_KEY_NAME]: name,
+// 			[rhit.FB_KEY_AUTHOR]: rhit.fbAuthManager.uid,
+// 			[rhit.FB_KEY_LOCATION]: location,
+// 			[rhit.FB_KEY_URL]: url,
+// 		})
+// 		.then(() => {
+// 			console.log("Document successfully updated!");
+// 		})
+// 		.catch(function (error) {
+// 			console.error("Error updating document: ", error);
+// 		});
+// 	}
 
-	getAccount() {
-		const docSnapshot = this._documentSnapshots[index];
-		const mq = new rhit.Account(docSnapshot.id,
-			docSnapshot.get(rhit.FB_KEY_NAME),
-			docSnapshot.get(rhit.FB_KEY_URL),
-			docSnapshot.get(rhit.FB_KEY_LOCATION));
-		return mq;
-	}
+// 	getAccount() {
+// 		const docSnapshot = this._documentSnapshots[index];
+// 		const mq = new rhit.Account(docSnapshot.id,
+// 			docSnapshot.get(rhit.FB_KEY_NAME),
+// 			docSnapshot.get(rhit.FB_KEY_URL),
+// 			docSnapshot.get(rhit.FB_KEY_LOCATION));
+// 		return mq;
+// 	}
 
-	get name() {
-		return this._documentSnapshot.get(rhit.FB_KEY_NAME);
-	}
+// 	get name() {
+// 		return this._documentSnapshot.get(rhit.FB_KEY_NAME);
+// 	}
 
-	get location() {
-		return this._documentSnapshot.get(rhit.FB_KEY_LOCATION);
-	}
+// 	get location() {
+// 		return this._documentSnapshot.get(rhit.FB_KEY_LOCATION);
+// 	}
 
-	get url() {
-		return this._documentSnapshot.get(rhit.FB_KEY_URL);
-	}
-}
+// 	get url() {
+// 		return this._documentSnapshot.get(rhit.FB_KEY_URL);
+// 	}
+// }
+// 		$("#editAccountDialog").on("show.bs.modal", (event) => {
+// 			// Pre animation
+// 			document.querySelector("#editInputName").value = rhit.fbAccountManager.name;
+// 			document.querySelector("#editInputLocation").value = rhit.fbAccountManager.location;
+// 			document.querySelector("#editInputURL").value = rhit.fbAccountManager.url;
+// 		});
+// 		$("#editAccountDialog").on("shown.bs.modal", (event) => {
+// 			// Post animation
+// 			document.querySelector("#editInputName").focus();
+// 		});
+
+// 		rhit.fbAccountManager.beginListening(this.updateView.bind(this));
+
+// 	}
+// 	updateView() {
+// 		const newList = htmlToElement('<div id="accountPage"></div>');
+// 		const mq = rhit.fbAccountManager.getAccount();
+// 		const newCard = this._createCard(mq);
+// 		newList.appendChild(newCard);
+
+// 		const oldList = document.querySelector("#accountPage");
+// 		oldList.removeAttribute("id");
+// 		oldList.hidden = true;
+// 		oldList.parentElement.appendChild(newList);
+
+// 		document.querySelector("#accountUrl").innerHTML = `<img class="img-fluid card-img-top" style="margin: 20px; max-height: 375px;" src="${rhit.fbAccountManager.url}">`;
+// 		document.querySelector("#accountName").innerHTML = rhit.fbAccountManager.name;
+// 		document.querySelector("#accountEmail").innerHTML = rhit.fbAccountManager.email;
+// 		document.querySelector("#accountSLocation").innerHTML = rhit.fbAccountManager.url;
+// 	}
+// 	_createCard(account) {
+// 		return htmlToElement(`<div class="card mx-auto profile-style">
+// 		<div class="card-body">
+// 			<div id="accountUrl"><img style="margin: 20px; max-height: 375px;" class="card-img-top"
+// 	  src="${account.url}"
+// 	  alt="Your profile picture"></div>
+// 			<h4 id="${account.name}"></h4>
+// 			<p id="${account.id}@rose-hulman.edu" class="card-text"></p>
+// 			<p id="${account.location}" class="card-text"></p>
+// 		</div>
+// 	</div>`);
+// 	}
+
+// }
+
+// rhit.Account = class {
+// 	constructor(name, location, url){
+// 		this.name = name;
+// 		this.url = url;
+// 		this.location = location;
+// 	}
+// }
+
+// rhit.FbAccountManager = class {
+// 	constructor(uid) {
+// 		rhit.HandleDrawerButtons();
+// 		this._documentSnapshot = {};
+// 		this._unsubscribe = null;
+// 		this._ref = firebase.firestore().collection(rhit.FB_USERS_COLLECTION.doc(uid));
+// 	}
+
+// 	beginListening(changeListener) {
+// 		this._unsubscribe = this._ref.onSnapshot((doc) => {
+// 			if (doc.exists) {
+// 				console.log("Document data:", doc.data());
+// 				this._documentSnapshot = doc;
+// 				changeListener();
+// 			} else {
+// 				console.log("No such document!");
+// 			}
+// 		});
+// 	}
+
+// 	stopListening() {
+// 		this._unsubscribe();
+// 	}
+
+// 	add(name, location, url) {
+// 		this._ref.add({
+// 			[rhit.FB_KEY_NAME]: name,
+// 			[rhit.FB_KEY_AUTHOR]: rhit.fbAuthManager.uid,
+// 			[rhit.FB_KEY_LOCATION]: location,
+// 			[rhit.FB_KEY_URL]: url,
+// 		})
+// 		.then(() => {
+// 			console.log("Document successfully updated!");
+// 		})
+// 		.catch(function (error) {
+// 			console.error("Error updating document: ", error);
+// 		});
+// 	}
+
+// 	getAccount() {
+// 		const docSnapshot = this._documentSnapshots[index];
+// 		const mq = new rhit.Account(docSnapshot.id,
+// 			docSnapshot.get(rhit.FB_KEY_NAME),
+// 			docSnapshot.get(rhit.FB_KEY_URL),
+// 			docSnapshot.get(rhit.FB_KEY_LOCATION));
+// 		return mq;
+// 	}
+
+// 	get name() {
+// 		return this._documentSnapshot.get(rhit.FB_KEY_NAME);
+// 	}
+
+// 	get location() {
+// 		return this._documentSnapshot.get(rhit.FB_KEY_LOCATION);
+// 	}
+
+// 	get url() {
+// 		return this._documentSnapshot.get(rhit.FB_KEY_URL);
+// 	}
+// }
 
 
 rhit.ForumListController = class {
@@ -568,11 +681,11 @@ function initMap(pointAGiven, pointBGiven) {
 	});
 }
 
-rhit.AccountPageController = class {
-	constructor() {
-		rhit.HandleDrawerButtons();
-	}
-}
+// rhit.AccountPageController = class {
+// 	constructor() {
+// 		rhit.HandleDrawerButtons();
+// 	}
+// }
 
 rhit.FbAuthManager = class {
 	constructor() {
@@ -664,8 +777,8 @@ rhit.initializePage = function () {
 	}
 	if (document.querySelector("#accountPage")) {
 		console.log("You are on the profile page.");
-		rhit.fbAccountManager = new rhit.FbAccountManager(rhit.fbAuthManager.uid);
-		new rhit.AccountPageController();
+		rhit.ProfilePageController = new rhit.FbAccountManager(rhit.fbAuthManager.uid);
+		new rhit.FbAccountManager();
 	}
 	if (document.querySelector("#findRoutePage")) {
 		console.log("You are on the find route page.");
